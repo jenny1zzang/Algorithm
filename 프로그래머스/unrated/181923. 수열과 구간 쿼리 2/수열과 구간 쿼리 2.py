ac81f4f24cab9e968498c58595d13aee2e4714ea
Kim Haerin <108577676+jenny1zzang@users.noreply.github.com>
2023-06-27 23:20:26 +0900

    [unrated] Title: 수열과 구간 쿼리 2, Time: 7.37 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
+++ 프로그래머스/unrated/181923. 수열과 구간 쿼리 2/수열과 구간 쿼리 2.py
@@ -1,12 +1,9 @@
 def solution(arr, queries):
     answer = []
     for s, e, k in queries:
-        min = 1000001
-        for i in range(len(arr)):
-            if i>=s and i<=e and arr[i]>k and min > arr[i]:
-                    min = arr[i]
-        if min == 1000001:
-            answer.append(-1)
-        else:
-            answer.append(min)
+        tmp = []
+        for i in arr[s:e+1]:
+            if i > k:
+                tmp.append(i)
+        answer.append(-1 if not tmp else min(tmp))
     return answer
